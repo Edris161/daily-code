cb0200fd1b250a282d229e587c4131cd13245ac2
Edris161 <edrisalowdin2022@gmail.com>
2025-09-13 10:05:55 +0430

    quize project
    trying to move forwarde

--- Python/Quize/main.py
+++ Python/Quize/main.py
@@ -1,5 +1,7 @@
 from question_modul import Question
 from data import question_data
+from brain_quize import Quizebrain
+
 
 question_bank = []
 for question in question_data:
@@ -7,3 +9,9 @@ for question in question_data:
         question_answer = question["answer"]
         new_question = Question(question_text, question_answer)
         question_bank.append(new_question)
+
+quize = Quizebrain(question_bank)
+
+
+while #quize.still_has_questions():
+        quize.next_question()
